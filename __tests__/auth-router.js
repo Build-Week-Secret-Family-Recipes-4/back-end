559994beaca497787fa903848407b9d3d8799ba3
April Ma <54913304+april5622@users.noreply.github.com>
2020-05-21 18:16:33 -0700

    latest save for deployment

---
+++ __tests__/auth-router.js
@@ -0,0 +1,40 @@
+const supertest = require("supertest")
+const server = require("../api/server")
+const db = require("../database/dbConfig")
+
+beforeAll(async () => {
+    await db('users').truncate()
+    await db.migrate.latest()
+    await db.seed.run()
+  })
+afterAll(async () => {
+    await db.destroy()
+
+})
+
+describe("auth integration tests", () => {
+    it("POST /register SHOULD SUCCEED", async () =>{
+        const data = {firstname: "bob",
+        lastname: "builder",
+        email: "bobthebuilder@gmail.com",
+        username: "bobthebuilder",
+        password: "passwordbuild"
+       }
+       const res = await supertest(server).post("/auth/register").send(data)
+       expect(res.statusCode).toBe(201)
+       expect(res.type).toBe("application/json")
+       expect(res.body.username).toBe("bobthebuilder")
+    })
+
+    it("POST /register SHOULD FAIL", async () =>{
+        const data = {firstname: "bob",
+        lastname: "builder",
+        email: "bobthebuilder@gmail.com",
+        username: "bobthebuilder",
+        password: "passwordbuild"
+       }
+       const res = await supertest(server).post("/auth/register").send(data)
+       expect(res.statusCode).toBe(409)
+       
+    })
+})
