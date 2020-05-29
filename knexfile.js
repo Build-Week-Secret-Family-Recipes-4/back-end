57ccaa04b1b6073c8167b70b798ec55869d3b10e
April Ma <54913304+april5622@users.noreply.github.com>
2020-05-28 19:17:13 -0700

    production file included

--- knexfile.js
+++ knexfile.js
@@ -37,4 +37,22 @@ module.exports = {
       },
     },
   },
-}
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/recipes.db3',
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      },
+    },
+  },
+};
